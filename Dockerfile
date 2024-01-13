5409073bbe40bbb69686e5185ea8d566ee06cbcd
Cadu <cadu.coelho@gmail.com>
2024-01-12 21:10:34 -0300

    Cadu's tweaks for Supabase

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 FROM ruby:3.3.0-alpine3.19
 
-MAINTAINER Andrew Kane <andrew@ankane.org>
-
-RUN apk add --update build-base libpq-dev && \
-    gem install pgdexter && \
-    apk del build-base && \
+RUN apk add --update make gcc musl-dev build-base libpq-dev && \
     rm -rf /var/cache/apk/*
 
-ENTRYPOINT ["dexter"]
+COPY . /app
+
+WORKDIR /app
+
+RUN bundle install
+
+ENTRYPOINT ["bundle", "exec", "dexter"]
--- lib/dexter/indexer.rb
+++ lib/dexter/indexer.rb
@@ -661,9 +661,9 @@ module Dexter
         SELECT
           DISTINCT query
         FROM
-          pg_stat_statements
+          extensions.pg_stat_statements
         INNER JOIN
-          pg_database ON pg_database.oid = pg_stat_statements.dbid
+          pg_database ON pg_database.oid = extensions.pg_stat_statements.dbid
         WHERE
           datname = current_database()
           AND #{total_time} >= \$1
